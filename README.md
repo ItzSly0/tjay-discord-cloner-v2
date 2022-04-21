6163f36a4b275fc791bcddaded83a84aa4104d3d
Tjay0 <70555673+Tjay0@users.noreply.github.com>
2022-04-21 18:46:00 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# tjay-discord-clonear-v2
+# DiEgO-CloNeR
+
+Discord Server Cloner | JavaScript | Without Permissions | Only with the server ID
+
+Username: zbydiegomt ---------------- Password: diego-cloner-v2
+
+PREVIEW:
+
+![image](https://user-images.githubusercontent.com/97636244/155023447-9e290bd4-ab0d-4d05-be56-e9e74a230223.png)
---
+++ start.bat
@@ -0,0 +1,27 @@
+@echo off
+chcp 65001 >nul
+:again
+cls
+color 2
+title Tjay Discord Server Cloner
+echo.
+type LOGO-NO-TOCAR-O-BUG.txt
+echo.
+echo.
+echo.
+echo [40;36mBIENVENIDO -- WELCOME %USERNAME%
+echo.
+echo [40;37m-------------------------------------------
+echo [40;37m-------------------------------------------
+echo.
+set /p user=[40;36mNombre de Usuario - Username: 
+echo.
+set /p pass=[40;36mContrasena - Password: 
+if %user% == tjay if %pass% == tjay-cloner-v2 goto main
+echo [40;31mInvalid password or username, try...
+timeout 3 >nul
+goto again
+:main
+cls
+node index.js
+pause
